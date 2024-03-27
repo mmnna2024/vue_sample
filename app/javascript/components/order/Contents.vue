0afae4c49c72407703438fea8f5096fcc6a4415b
naomu <muramatsu2023@gmail.com>
2024-03-27 18:39:20 +0900

    add:dispray price and add button

--- app/javascript/components/order/Contents.vue
+++ app/javascript/components/order/Contents.vue
@@ -1,11 +1,13 @@
 <template>
   <div>
+    <button @click="increment">+</button>
+    <button @click="decrement">-</button>
     <table class="contents-table">
       <tr>
         <th>衣類選択</th>
         <th>料金</th>
       </tr>
-      <tr>
+      <tr v-for="(_) in counter">
         <td>
           <select v-model="selected">
             <option disabled value="">依頼する衣類を一つずつお選びください</option>
@@ -32,14 +34,24 @@ export default {
   },
   data() {
     return {
-      selected: ''
+      selected: '',
+      counter: 3
     };
   },
-  methods: {
-    SelectedPrice: function() {
-      const item = this.content.find(({name}) => name === this.selected);
+  computed: {
+    SelectedPrice: function () {
+      if (!this.selected) return 0;
+      const item = this.contents.find(({ name }) => name === this.selected);
       return item ? item.price : 0;
     },
+  },
+  methods: {
+    increment() {
+      this.counter++;
+    },
+    decrement() {
+      this.counter--;
+    }
   },
 };
 </script>
